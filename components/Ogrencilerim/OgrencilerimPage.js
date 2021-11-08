010e77ac35fab2c3ee24b5e1d7d4523d9bba1ba3
saeid <saeid.noormohammad@gmail.com>
2021-11-08 19:31:07 +0300

    here we go

--- components/Ogrencilerim/OgrencilerimPage.js
+++ components/Ogrencilerim/OgrencilerimPage.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Ogrenciler from "./Ogrenciler/Ogrenciler";
 import ogrencilerim from "./ogrencilerim.module.css";
 
 export default function OgrencilerimPage() {
-  return <div className={ogrencilerim.mainContainer}></div>;
+  return (
+    <div className={ogrencilerim.mainContainer}>
+      <Ogrenciler />
+    </div>
+  );
 }
