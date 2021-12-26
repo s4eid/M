39a4191775f55f643706d01d664de98d235afb58
saeid <saeid.noormohammad@gmail.com>
2021-12-26 18:24:29 +0300

    d

--- components/LoginTeacher/FormContainer.js
+++ components/LoginTeacher/FormContainer.js
@@ -21,6 +21,7 @@ export default function FormContainer() {
       Cookies.set("refresh", data.loginTeacher.refreshToken, {
         expires: 1,
         secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
       });
       route.push("/teacher/explore");
     },
